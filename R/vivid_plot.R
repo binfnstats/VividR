800bca640f05b063b94e7a91721dce0f9d795637
SmithConnor <connorsmith010@gmail.com>
2021-07-14 15:38:43 +1000

    Changes to plot function

--- R/vivid_plot.R
+++ R/vivid_plot.R
@@ -22,81 +22,6 @@
 #'
 #' vivid_plot(vividObj = vividResults)
 
-
-vivid_plot = function(vividObj, log = TRUE) {
-  vividSplit  = vividObj$vividSplit
-  if (vividSplit == TRUE) {
-    vividObj = vividObj[[length(vividObj)]]
-  }
-
-  vividObj$varClust = dendsort::dendsort(d = vividObj$varClust)
-
-  vividObj$varClust$labels = stringr::str_trunc(
-    string = colnames(vividObj$varClust$labels),
-    width = 12,
-    side = "right"
-  )
-
-  ddRow = stats::as.dendrogram(vividObj$varClust)
-  rowOrd = stats::order.dendrogram(ddRow)
-  ddCol = ddRow
-  colOrd = rowOrd
-
-  colnames(vividObj$varMat) = stringr::str_trunc(
-    string = colnames(vividObj$varMat),
-    width = 12,
-    side = "right"
-  )
-  rownames(vividObj$varMat) = stringr::str_trunc(
-    string = rownames(vividObj$varMat),
-    width = 12,
-    side = "right"
-  )
-
-  cc = grDevices::colorRampPalette(c("darkgreen", "green", "yellow", "red", "darkred"))
-  lattice::trellis.par.set(regions = list(col = cc(100)))
-
-
-  base::switch(
-    log,
-    "TRUE" = lattice::levelplot(xlab = list(label = "Features", cex = 1.5),
-                                ylab = list(label = "Features", cex = 1.5),
-                                main = "Sacurine MPP",
-                                vividObj$varMat[rowOrd[1:25], colOrd[1:25]],
-                                col.regions = cc,
-                                scales = list(cex = 0.5, x = list(rot =
-                                                                    90)),
-                                aspect = "fill",
-                                colorkey = list(space = "left"),
-                                scales=list(tck=c(1,0), x=list(cex=1.2), y=list(cex=1.5))
-    )
-    ,
-    "FALSE" = lattice::levelplot(xlab = "Features",
-                                 ylab = "Features",
-                                 vividObj$varMat[rowOrd, colOrd],
-                                 col.regions = cc,
-                                 scales = list(cex = 0.5, x = list(rot =
-                                                                     90)),
-                                 aspect = "fill",
-                                 colorkey = list(space = "left"),
-                                 legend = list(right = list(
-                                   fun = latticeExtra::dendrogramGrob,
-                                   args = list(
-                                     x = ddCol,
-                                     ord = colOrd,
-                                     side = "right",
-                                     size = 6
-                                   )
-                                 ))
-    )
-  )
-
-}
-
-
-
-#########################
-
 vivid_plot = function(vividObj, log = TRUE, topN = 0) {
   vividSplit  = vividObj$vividSplit
   if (vividSplit == TRUE) {
@@ -141,7 +66,8 @@ vivid_plot = function(vividObj, log = TRUE, topN = 0) {
     log,
     "TRUE" = lattice::levelplot(xlab = "Features",
                                 ylab = "Features",
-                                vividObj$varMat[rowOrd, colOrd],
+                                main = "MVP",
+                                log(vividObj$varMat[rowOrd[1:topN], colOrd[1:topN]]+1),
                                 col.regions = cc,
                                 scales = list(cex = 0.5, x = list(rot =
                                                                     90)),
@@ -159,7 +85,8 @@ vivid_plot = function(vividObj, log = TRUE, topN = 0) {
     ),
     "FALSE" = lattice::levelplot(xlab = "Features",
                                  ylab = "Features",
-                                 vividObj$varMat[rowOrd, colOrd],
+                                 main = "MVP",
+                                 vividObj$varMat[rowOrd[1:topN], colOrd[1:topN]],
                                  col.regions = cc,
                                  scales = list(cex = 0.5, x = list(rot =
                                                                      90)),
--- README.md
+++ README.md
@@ -12,6 +12,37 @@ data.
 ``` r
 library(devtools)
 devtools::install_github("SmithConnor/VividR")
+install.packages('ropls')
+install.packages('furrr')
 ```
 
 ## A quick example
+
+``` r
+library('VIVID')
+library('ropls')
+
+data("sacurine") #Load sacurine dataset from the 'ropls' package
+
+dat <- sacurine$dataMatrix
+outcomes <- sacurine$sampleMetadata$gender
+
+vivid.sacurine <- vivid(x = dat,
+                        y = outcomes,
+                        bootstraps = 50,
+                        cores = parallel::detectCores() - 1,
+                        seed = 1234567,
+                        lambda = 'lambda.1se',
+                        compareMethod = 'BIC')
+```
+
+From the feature selection algorithm using the BIC we have selected:
+
+``` r
+vivid.sacurine$optFeatures
+#> [1] "X.gamma.Glu.Leu.Ile"          "Glu.Val"                     
+#> [3] "Gluconic.acid.and.or.isomers" "Malic.acid"                  
+#> [5] "N2.Acetylaminoadipic.acid"    "Oxoglutaric.acid"            
+#> [7] "p.Hydroxyhippuric.acid"       "Pantothenic.acid"            
+#> [9] "Testosterone.glucuronide"
+```
